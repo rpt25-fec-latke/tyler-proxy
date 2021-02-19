8316f45ad6b1e3be3816fc8ab82bc3b5aee02fc6
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-02-18 21:03:38 -0800

    Refactor proxy to pull from team members instances

--- index.js
+++ index.js
@@ -10,9 +10,37 @@ app.use(express.static(path.join(__dirname, '/./public')));
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  console.log(req.headers);
-  res.status(200);
+app.get('/reviews', async (req, res) => {
+  const id = req.query.id;
+
+  try {
+    const { data } = await axios.get(`http://204.236.178.72:3001/reviews/?id=${id}`);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get('/metadata', async (req, res) => {
+  const id = req.query.id;
+
+  try {
+    const { data } = await axios.get(`http://3.131.140.35:3005/metadata/?id=${id}`);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get('/game_carousel_info', async (req, res) => {
+  const id = req.query.id;
+
+  try {
+    const { data } = await axios.get(`http://3.137.75.100:3008/game_carousel_info/?id=${id}`);
+    res.json(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.listen(port, () => {
