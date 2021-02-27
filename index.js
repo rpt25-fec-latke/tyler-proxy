d46ee00530f362061c17992cf69bd8228a9b10fa
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-02-26 19:08:41 -0800

    Add other services to server routes

--- index.js
+++ index.js
@@ -43,6 +43,30 @@ app.get('/game_carousel_info', async (req, res) => {
   }
 });
 
+app.get('/morelikethis', async (req, res) => {
+  const gameId = req.query.id;
+  //3.137.75.100
+  try {
+    const { data } = await axios.get(`http://3.137.75.100:3002/morelikethis?id=${gameId}`)
+    res.json(data);
+  } catch(err) {
+    console.log(err);
+    res.status(500).send({ internalServerError: err });
+  }
+});
+
+app.get('/events_and_announcements', async (req, res) => {
+  const id = req.query.id;
+
+  try {
+    const { data } = await axios.get(`http://204.236.178.72:3007/events_and_announcements/?id=${id}`);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
